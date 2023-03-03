c5fdf1fe2f0f615ec3805a2fb724e2ee8c35ed90
moon <102299695+onomlight@users.noreply.github.com>
2023-03-03 11:31:14 +0900

    Add files via upload

---
+++ bassics.ts
@@ -0,0 +1,25 @@
+//Primitives : number, string, boolean
+// More complex types : arrays, objects
+//Function types, parameters
+
+
+
+// primitives 기본타입
+
+let age : number;
+//이러면 변수 선언끝
+
+age = 12; // 함수와 매개 변수여서 '' 사용어려움
+
+let userName : string; //뮨자욜
+//타입스크립트는 타입표기는 소문자로 시작함 java처럼 대문자아님
+
+userName = 'Max'
+
+let isInstructor : boolean; // 변수 생성 후 타입을 지정하면 논리값 저장
+isInstructor = true
+
+// let hobbies : null;
+
+// hobbies = 12 ;  
+// 숫자형 문자열 논리형 주요 기본형 타입
