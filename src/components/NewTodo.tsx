cee5fed9705124b4d3c94b2c093cd7ffcbc291e6
onomlight <pass041@nate.com>
2023-03-13 11:15:11 +0900

    refs 및 useRef 작업하기

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,11 +1,23 @@
+import { useRef } from "react";
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+  // useRef 자체가 제네릭 타입을 이용해 사용가능함
+  // 오류가 계속 뜨는 이유는 기본값을 직접 설정하지 않았기 때문임
+  // 다른 요소가 할당되어있을때 자동지정해야함 / 만근게 없음으로 null로 입력을 하면 ref에 오류가 사라짐
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enterdText = todoTextInputRef.current!.value;
+    //submitHandler 안에 enteredText 가져올수있음
+    if (enterdText.trim().length === 0) {
+      //throw an error
+      return;
+    }
   };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
