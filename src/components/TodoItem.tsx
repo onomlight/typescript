de48f4fcb5213a7f58a0f728a47c2fffbea2eafe
onomlight <pass041@nate.com>
2023-03-14 10:28:17 +0900

    : Todo 제거하기

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,15 @@
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}> {props.text}</li>;
+const TodoItem: React.FC<{
+  text: string;
+  onRemoveTodo: () => void; // (event: React.MouseEvent) 인수는 선택사항임으로 입력안해도됨!
+}> = (props) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {" "}
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
