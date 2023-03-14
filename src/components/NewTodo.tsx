69f80a4be8c9ed0359f078bf1c74bd4f5d3feb0f
onomlight <pass041@nate.com>
2023-03-14 10:18:37 +0900

    css 입히기

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import classes from "./NewTodo.module.css";
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   //react.fc<{}> => 제네릭 타입특성을 이용해서 프로퍼티 객체를 구체적으로 정의함
   //이후에 함수를 정의하여 함수타입으로 한 후 화살표 함ㄴ수 작성함
@@ -19,7 +20,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     props.onAddTodo(enterdText);
   };
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
